8447bc2b733ef6ff18d3ff25bbf445bb3c9b9f86
nhrajput <66969972+nhrajput@users.noreply.github.com>
2021-05-03 14:19:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+// CHapter 43 to 48
+
+
+// task 01
+// function welcome(){
+//     alert("Welcome here!");
+// }
+
+
+// task 02
+//  function greet(){
+//     alert("Thanks for purchasing a phone from us.")
+// }
+
+// task 03
+// function makeInvisible() {
+//     document.getElementById("noNeed" || "noNeeded" || "noNeeding").className = "hidden";
+//      }
+
+
+// task 05
+// var click = +0;
+//     function clicks() {
+//         click += 1;
+//         document.getElementById("clicks").innerHTML = click;
+//     };
+// var click1 =  +0;
+// function clicks1() {
+//     click1 -= 1;
+//     document.getElementById("clicks1").innerHTML = click1;
+// };
+
+
+// Chapter 49 to 52
+
+// task 01
+
+// function displayFields(){
+//     var a = document.getElementById('val1').value;
+//     var b = document.getElementById('val2').value;
+//     var c = document.getElementById('val3').value;
+//     document.write(a+"<br>"+b+"<br>"+c+"<br>")
+// }
+
+// task 02
+
+// var a = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit cupiditate ratione tempora consequuntur excepturi, veritatis quasi itaque quis labore vitae optio numquam cum ea, amet quam porro veniam quas, provident perferendis expedita. Eaque, nostrum?';
+// var b = document.getElementById('text');
+// b.innerHTML = a.slice(0,20)+'...<button onClick="fullText()">Read More </button>'
+
+
+// function fullText(){
+//     b.innerHTML = a;
+
+// }
+
+// task 03
+// function dataDisplay(){
+// var a = document.getElementById('name').value;
+//     var b = document.getElementById('pass').value;
+//     var c = document.getElementById('phone').value;
+//     document.write('<table><tr id="noNeed"><td> '+a+ '</td><td><button onClick="makeInvisible()">Delete</button></td><tr id="noNeeded"><td> '+b+ "</td><td><button onclick='makeInvisible()'>Delete</button></td><tr id='noNeeding'><td> "+c+ "</td><td><button onclick='makeInvisible()'>Delete</button></td>")
+// }
+
+// function makeInvisible() {
+//         document.getElementById("noNeed").className = "hidden";
+//          }
